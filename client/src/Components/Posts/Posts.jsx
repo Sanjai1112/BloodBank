e740e53cceded529c2eeb3e1f37d776c205b1e9f
dinesh <DineshJas>
2019-03-21 00:53:47 +0530

    added styles to posts

--- client/src/Components/Posts/Posts.jsx
+++ client/src/Components/Posts/Posts.jsx
@@ -5,7 +5,8 @@ class Posts extends Component {
   state = {
     fetchedDatas: [],
     isError: false,
-    errorMessage: ""
+    errorMessage: "",
+    showFullMessageToggle: false
   };
   componentWillMount() {
     console.log("Posts Component is called");
@@ -20,6 +21,10 @@ class Posts extends Component {
         this.setState({ isError: true, errorMessage: err.message });
       });
   }
+  handleClick = () => {
+    console.log("Toggle is called");
+    this.setState({ showFullMessageToggle: !this.state.showFullMessageToggle });
+  };
   render() {
     console.log("Posts called");
     console.log(typeof this.state.fetchedDatas);
@@ -28,11 +33,37 @@ class Posts extends Component {
       <div>
         {this.state.fetchedDatas.map(data => {
           return (
-            <div key={data._id} className="posts">
+            <div
+              key={data._id}
+              className="posts"
+              onClick={this.handleClick}
+              style={
+                this.state.showFullMessageToggle
+                  ? { height: "110px" }
+                  : { height: "60px" }
+              }
+            >
               <h4>
                 <span className="posts-title_head"> Patient Name</span>:
                 <span className="posts-title_name">{data.patientName}</span>
               </h4>
+              <div className="posts-details">
+                <h4>
+                  <span className="posts-title_head"> Number</span>:
+                  <span className="posts-title_name">{data.contactNumber}</span>
+                </h4>
+                <h4>
+                  <span className="posts-title_head">Group</span>:
+                  <span className="posts-title_name">{data.bloodGroup}</span>
+                </h4>
+              </div>
+              <address>
+                <span className="posts-title_head">Address</span>:{data.address}
+              </address>
+              <p className="posts-message">
+                <span className="posts-title_head">Message</span>
+                {data.additionalMessage}
+              </p>
             </div>
           );
         })}
